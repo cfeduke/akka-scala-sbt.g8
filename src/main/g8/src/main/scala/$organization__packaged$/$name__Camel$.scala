2429b4211b1c95d315f73e9123200d7062d2819f
Charles Feduke <charles.feduke@gmail.com>
2014-02-17 20:12:49 -0500

    fixes for g8 template processing

--- src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name__Camel$.scala
@@ -19,7 +19,7 @@ class Counter extends Actor {
 class Reporter(val printer: Any => Unit) extends Actor {
   def receive = {
     case (count) =>
-      printer("Count:\t\t{}".format(count))
+      printer("Count:\t%s".format(count))
   }
 }
 
@@ -27,12 +27,12 @@ class RunLoop extends Actor {
   import context.dispatcher
 
   val counter = context.actorOf(Props(classOf[Counter]))
-  val reporter = context.actorOf(Props(classOf[Reporter], println))
+  val reporter = context.actorOf(Props(classOf[Reporter], (msg: Any) => println(msg)))
 
   context.system.scheduler.schedule(1 second, 1 second, counter, Tick)
   context.system.scheduler.schedule(3 seconds, 3 seconds, counter, Get)(context.dispatcher, reporter)
 
-  case receive = {
+  def receive = {
     case _ =>
   }
 }
